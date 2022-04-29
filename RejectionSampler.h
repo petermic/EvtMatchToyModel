5644f915f02689c5bc4a67d9f4c4ce7a25d1d2b5
petermic <mjpianist.000@gmail.com>
2022-04-29 11:19:49 -0400

    initial commit

---
+++ RejectionSampler.h
@@ -0,0 +1,62 @@
+#include "TH1.h"
+#include "TRandom.h"
+
+class RejectionSampler {
+  public:
+  RejectionSampler(TH1F* dist);
+  ~RejectionSampler(){}
+  float getFloatValue();
+  int getIntValue();
+  private:
+  float getValue();
+  bool isInsideHistogram(float value);
+  TH1F* d;
+  TRandomMixMax r;
+  bool int_flag;
+};
+
+RejectionSampler::RejectionSampler(TH1F* dist){
+  d = dist;
+}
+
+float RejectionSampler::getFloatValue(){
+  int_flag = false;
+  return getValue();
+}
+
+int RejectionSampler::getIntValue(){
+  int_flag = true;
+  return round(getValue());
+}
+
+float RejectionSampler::getValue(){
+  // form box
+  float min_x = d->GetBinLowEdge(1); // ignore underflow bin
+  int nbins = d->GetNbinsX();
+  float max_x = d->GetBinLowEdge(nbins)+d->GetBinWidth(nbins); // ignore overflow bin
+  if(d->GetMinimum()<0) std::cout << "RejectionSampler: !!WARNING!! Negative bin content detected. Sampler may not work correctly!" << std::endl;
+  float min_y = 0.; // rejection sampling cannot deal with negative bin content
+  float max_y = d->GetMaximum();
+  
+  
+  float vx;
+  float vy;
+  bool found = false;
+  while(!found){
+    // generate random point
+    if(int_flag){
+      int min_int = round(min_x);
+      int randint = r.Integer(round(max_x))+min_int;
+      vx = (float)randint;
+    }
+    else{
+      vx = r.Uniform(max_x)+min_x;
+    }
+    vy = r.Uniform(max_y);
+  
+    // check whether point is inside histogram
+    float hval = d->GetBinContent(d->FindBin(vx));
+    if(vy<hval) found = true;
+  }
+  return vx;
+}
---
+++ samplertest.C
@@ -0,0 +1,125 @@
+#include "RejectionSampler.h"
+#include "TRandom.h"
+
+void samplertest(){
+  TFile* f = TFile::Open("/raid5/data/wangj/home/phoDcorrRun2018/mcsub/rootfiles/hydjet_hydjet__PbPb_MC/savehist.root");
+  TH1F* nD = (TH1F*)f->Get("hnG");
+  TH1F* njt = (TH1F*)f->Get("hnjt");
+  RejectionSampler rD(nD);
+  RejectionSampler rjt(njt);
+  TRandomMixMax rpythia;
+  TRandomMixMax rphi;
+  double nD_coeff = 1./2.;
+  double njt_coeff = 1./5.;
+
+  unsigned int n_evts = 1000000;
+  // e = embed, m = mix
+  TH1F* npairs_ee = new TH1F("npairs_ee","n_pairs (Pythia+HYDJET D, Pythia+HYDJET jet)",41,-20,20);
+  TH1F* npairs_em = new TH1F("npairs_em","n_pairs (Pythia+HYDJET D, HYDJET jet)",41,-20,20);
+  TH1F* npairs_me = new TH1F("npairs_me","n_pairs (HYDJET D, Pythia+HYDJET jet)",41,-20,20);
+  TH1F* npairs_mm = new TH1F("npairs_mm","n_pairs (HYDJET D, HYDJET jet)",41,-20,20);
+  TH1F* npairs_pythia = new TH1F("npairs_pythia","n_pairs (Pythia)",41,-20,20);
+
+  TH1F* dphi_ee = new TH1F("dphi_ee","dphi (Pythia+HYDJET D, Pythia+HYDJET jet)",100,0,M_PI);
+  TH1F* dphi_em = new TH1F("dphi_em","dphi (Pythia+HYDJET D, HYDJET jet)",100,0,M_PI);
+  TH1F* dphi_me = new TH1F("dphi_me","dphi (HYDJET D, Pythia+HYDJET jet)",100,0,M_PI);
+  TH1F* dphi_mm = new TH1F("dphi_mm","dphi (HYDJET D, HYDJET jet)",100,0,M_PI);
+  TH1F* dphi_pythia = new TH1F("dphi_pythia","dphi (Pythia)",100,0,M_PI);
+  for(int i=0;i<n_evts;i++){
+    int nD_mb = rD.getIntValue();
+    int njt_mb = rjt.getIntValue();
+    int nD_pythia = round(rpythia.Exp(nD_coeff));
+    int njt_pythia = round(rpythia.Exp(njt_coeff));
+    int nD_mix = rD.getIntValue();
+    int njt_mix = rjt.getIntValue();
+    
+    int np_ee = (nD_mb + nD_pythia) * (njt_mb + njt_pythia);
+    int np_em = (nD_mb + nD_pythia) * njt_mix;
+    int np_me = nD_mix * (njt_mb + njt_pythia);
+    int np_mm = nD_mix * njt_mix;
+    int np_pythia = nD_pythia * njt_pythia;
+
+    for(int j=0;j<np_ee;j++){
+      dphi_ee->Fill(rphi.Uniform(M_PI));
+    }
+    for(int j=0;j<np_em;j++){
+      dphi_em->Fill(rphi.Uniform(M_PI));
+    }
+    for(int j=0;j<np_me;j++){
+      dphi_me->Fill(rphi.Uniform(M_PI));
+    }
+    for(int j=0;j<np_mm;j++){
+      dphi_mm->Fill(rphi.Uniform(M_PI));
+    }
+    for(int j=0;j<np_pythia;j++){
+      dphi_pythia->Fill(rphi.Uniform(M_PI));
+    }
+
+    npairs_ee->Fill(np_ee);
+    npairs_em->Fill(np_em);
+    npairs_me->Fill(np_me);
+    npairs_mm->Fill(np_mm);
+    npairs_pythia->Fill(np_pythia);
+  }
+
+  TH1F* dphi_sub = new TH1F("dphi_sub","dphi (subtracted)",100,0,M_PI);
+  dphi_sub->Add(dphi_ee,dphi_em,1.,-1.);
+  dphi_sub->Add(dphi_me,-1.);
+  dphi_sub->Add(dphi_mm);
+
+  gStyle->SetOptStat(0);
+
+  TCanvas* c = new TCanvas("c","c",800,600);
+  c->SetLogy();
+
+  npairs_ee->SetMinimum(10);
+  npairs_ee->SetMaximum(pow(10,7));
+  npairs_ee->SetTitle("");
+
+  npairs_ee->SetLineColor(kBlack);
+  npairs_em->SetLineColor(kBlue);
+  npairs_me->SetLineColor(kGreen+2);
+  npairs_mm->SetLineColor(kViolet);
+  npairs_pythia->SetLineColor(kRed);
+
+  npairs_ee->Draw("E");
+  npairs_em->Draw("Esame");
+  npairs_me->Draw("Esame");
+  npairs_mm->Draw("Esame");
+  npairs_pythia->Draw("Esame");
+
+  TLegend* legend = new TLegend(0.8,0.8,1.,1.);
+  legend->AddEntry(npairs_ee);
+  legend->AddEntry(npairs_em);
+  legend->AddEntry(npairs_me);
+  legend->AddEntry(npairs_mm);
+  legend->Draw("same");
+
+  TCanvas* d = new TCanvas("d","d",800,600);
+  
+  dphi_ee->SetMinimum(0);
+  dphi_ee->SetTitle("");
+
+  dphi_ee->SetLineColor(kBlack);
+  dphi_em->SetLineColor(kBlue);
+  dphi_me->SetLineColor(kGreen+2);
+  dphi_mm->SetLineColor(kViolet);
+  dphi_pythia->SetLineColor(kRed);
+  dphi_sub->SetLineColor(kOrange);
+
+  dphi_ee->Draw("E");
+  dphi_em->Draw("Esame");
+  dphi_me->Draw("Esame");
+  dphi_mm->Draw("Esame");
+  dphi_pythia->Draw("Esame");
+  dphi_sub->Draw("Esame");
+
+  TLegend* dlegend = new TLegend(0.8,0.8,1.,1.);
+  dlegend->AddEntry(dphi_ee,"dphi (Pythia+HYDJET D, Pythia+HYDJET jet)");
+  dlegend->AddEntry(dphi_em);
+  dlegend->AddEntry(dphi_me);
+  dlegend->AddEntry(dphi_mm);
+  dlegend->AddEntry(dphi_pythia);
+  dlegend->AddEntry(dphi_sub);
+  dlegend->Draw("same");
+}
